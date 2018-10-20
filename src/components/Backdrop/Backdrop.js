d7aa4c614fe543a5385a1d83c0137fdf1c98ce48
Tommaso Ricci <zorgatone@gmail.com>
2018-10-20 16:50:46 +0200

    CSS animations

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -2,6 +2,10 @@ import React from "react";
 
 import "./Backdrop.css";
 
-const backdrop = props => <div className="Backdrop" />;
+const backdrop = props => {
+  const classes = ["Backdrop", props.show ? "BackdropOpen" : "BackdropClosed"];
+
+  return <div className={classes.join(" ")} />;
+};
 
 export default backdrop;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,13 +2,17 @@ import React from "react";
 
 import "./Modal.css";
 
-const modal = props => (
-  <div className="Modal">
-    <h1>A Modal</h1>
-    <button className="Button" onClick={props.closed}>
-      Dismiss
-    </button>
-  </div>
-);
+const modal = props => {
+  const classes = ["Modal", props.show ? "ModalOpen" : "ModalClosed"];
+
+  return (
+    <div className={classes.join(" ")}>
+      <h1>A Modal</h1>
+      <button className="Button" onClick={props.closed}>
+        Dismiss
+      </button>
+    </div>
+  );
+};
 
 export default modal;
